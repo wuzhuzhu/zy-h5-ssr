a786d5778d50b0e7a2339eabf54529a346bfc6b8
Walter Wu <tong.wu@8win.com>
2018-01-25 12:50:13 +0800

    kinds of stucked

--- conf/get-dva-app.js
+++ conf/get-dva-app.js
@@ -1,10 +1,25 @@
 import dva from 'dva-no-router';
+// redux persist
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+}
 
 // 1. Initialize
 const app = dva({
   onError(error) {
     // console.error(error)
     Toast.fail(error.message)
+  },
+  onReducer: reducer => {   // persist reducer enhencer
+    return (state, action) => {
+      return persistReducer(persistConfig, reducer)(state, action);
+      // 由于 dva 同步了 routing 数据，所以需要把这部分还原
+    }
   },
 });
 
@@ -13,15 +28,21 @@ const app = dva({
 
 // 3. Model
 
-export default (PageContainer, pageModel) => {
-  app.router(() => {
-    return <PageContainer />;
-  });
 
+
+export default (PageContainer, pageModel) => {
   try {
     app.model(pageModel)
   }
   catch(e) {}
+
+  // let persistor = persistStore(app._store)
+
+  app.router(() => {
+    return (
+        <PageContainer />
+    );
+  });
 
   return app.start();
 }
